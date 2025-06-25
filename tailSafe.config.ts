cf4db526b17d225524781df20ad0178286997675
Charl de Waal <charldewaal8@gmail.com>
2025-06-25 18:51:46 +0200

    prepare

---
+++ tailSafe.config.ts
@@ -0,0 +1,16 @@
+// tailSafe User Configuration
+import { TailSafeConfig } from "tailSafe";
+
+const tailSafeConfig: TailSafeConfig = {
+    aliases: {
+        // Example aliases
+
+        //align: ['flex', 'flex-row', 'items-center', 'justify-center', 'bg-red-500'],
+        //center: ['flex', 'items-center', 'justify-center'],
+        //card: ['bg-white', 'rounded-lg', 'shadow-md', 'p-6'],
+        //'btn-primary': ['bg-blue-500', 'text-white', 'px-4', 'py-2', 'rounded', 'hover:bg-blue-600']
+    },
+    theme: {}
+};
+
+export default tailSafeConfig;
