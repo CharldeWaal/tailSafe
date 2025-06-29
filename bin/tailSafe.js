e259a4c017bc17f7a95d3d70a3a32371f7d718f2
Charl de Waal <50237034+CharldeWaal@users.noreply.github.com>
2025-06-29 20:40:00 +0200

    TS-3: Update postinstall (#5)
    * user dir
    
    * update postinstall gen
    
    ---------
    
    Co-authored-by: Truné Snyman <trunesnyman@Trunes-MacBook-Air.local>

--- bin/tailSafe.js
+++ bin/tailSafe.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// The line above is the "shebang". It tells the system to execute this file with Node.js.
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const { execSync } = require('child_process');
@@ -7,20 +6,16 @@ const { execSync } = require('child_process');
 const path = require('path');
 
 // eslint-disable-next-line no-console
-console.log('⏳ Generating tailSafe files...');
+console.log('⏳ Generating tailsafe alias types...');
 
 try {
-    // Define the paths to your codegen scripts *relative to this file*.
     const codegenDir = path.resolve(__dirname, '../src/codegen');
 
     // Run the user alias script and generate module augmentation
     execSync(`node ${path.join(codegenDir, 'codegen.module.js')}`, { stdio: 'inherit' });
-    // Add any other scripts here...
 
     // eslint-disable-next-line no-console
-    console.log('🦈 tailSafe: All files have been generated successfully!');
-    // eslint-disable-next-line no-console
-    console.log('💡 You can now import: import { ProviderWrapper } from "tailsafe";');
+    console.log('🦈 tailsafe: All aliases have been generated successfully!');
 
 } catch (error) {
     // eslint-disable-next-line no-console
